c3a8416833c15753fce7ada1b10f6d54d5aee579
WajihulHassan944 <82878378+WajihulHassan944@users.noreply.github.com>
2023-07-01 19:12:24 +0500

    update 786 4

--- index.js
+++ index.js
@@ -1,98 +1,37 @@
+/** ------------------ IMPORTING PACKAGE ------------------ **/
 const express = require('express');
-const multer = require('multer');
-const csv = require('csv-parser');
-const fs = require('fs');
-const axios = require('axios');
-const mongodb = require('mongodb');
-
-const bodyParser = require('body-parser');
+const port = 8000;
 const app = express();
+const path = require('path');
 
+const expressLayouts = require('express-ejs-layouts');
+const csv = require('csv-parser');
+const db = require("./config/mongoose");
+const bodyParser = require('body-parser');
 
-const cors = require("cors");
-app.use(cors());
-app.use(express.json());
+// setting layouts
+app.use(expressLayouts);
 
+// middleware for body-parser
+app.use(bodyParser.json());
+app.use(express.urlencoded({extended: true}));
 
-const url = process.env.url ;
-const port = process.env.port || 3000;
-const MongoClient = mongodb.MongoClient;
-const dbName = 'test';
-const collectionName = 'helpers';
+//accesing static files from assets folder
+app.use(express.static('./assets'));    
 
-// Serve the HTML file
-app.get('/', (req, res) => {
-  res.send("Bismillah 786");
-});
-const storage = multer.diskStorage({
-    destination: 'uploads/',
-    filename: function (req, file, cb) {
-      cb(null, file.originalname);
+//setting up view engine
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+// setting up routes
+app.use('/', require('./routes'));
+
+// directing the app in the given port 
+app.listen(port, function(err) {
+    if(err) {
+        console.log('Error', err);
+        return;
     }
-  });
-  
-  const upload = multer({ storage: storage });
-// Handle CSV file upload and MongoDB insertion
-app.post('/upload', upload.single('csvFile'), (req, res) => {
-    const filePath = req.file.path;
-    const data = [];
-  
-    fs.createReadStream(filePath)
-      .pipe(csv())
-      .on('data', (row) => {
-        data.push(row);
-      })
-      .on('end', () => {
-        MongoClient.connect(url, (err, client) => {
-          if (err) {
-            console.error('Error connecting to MongoDB:', err);
-            res.status(500).send('Internal Server Error');
-            return;
-          }
-  
-          const db = client.db(dbName);
-          const collection = db.collection(collectionName);
-  
-          collection.insertMany(data, (err, result) => {
-            if (err) {
-              console.error('Error inserting data into MongoDB:', err);
-              res.status(500).send('Internal Server Error');
-              return;
-            }
-  
-            fs.unlinkSync(filePath); // Remove the temporary CSV file
-            res.send('CSV file uploaded and data inserted into MongoDB.');
-  
-            // Add an alert message
-            const script = `
-              <script>
-                alert('CSV file uploaded and data inserted into MongoDB.');
-              </script>
-            `;
-            res.write(script);
-            res.end();
-          });
-  
-          client.close();
-        });
-      });
-  });
-  
-// API endpoint to fetch the data from your backend API
-app.get('/data', (req, res) => {
-  const apiURL = 'https://backend-for-plans.onrender.com/api/helpers';
+    console.log('Server is up and running on port: ', port);
 
-  axios.get(apiURL)
-    .then(response => {
-      res.json(response.data);
-    })
-    .catch(error => {
-      console.error('Error fetching data:', error);
-      res.status(500).json({ error: 'Internal Server Error' });
-    });
-});
-
-// Start the server
-app.listen(port, () => {
-  console.log(`Server is running on ${port} `);
 });
