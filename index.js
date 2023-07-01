e6b408f951e88eee05ba7ea3e802ce438e4c94ae
WajihulHassan944 <82878378+WajihulHassan944@users.noreply.github.com>
2023-07-01 20:50:12 +0500

    update 786 5

--- index.js
+++ index.js
@@ -1,37 +1,106 @@
-/** ------------------ IMPORTING PACKAGE ------------------ **/
 const express = require('express');
-const port = 8000;
 const app = express();
 const path = require('path');
+const multer = require('multer');
+const fs = require('fs');
+const mongoose = require('mongoose');
 
-const expressLayouts = require('express-ejs-layouts');
-const csv = require('csv-parser');
-const db = require("./config/mongoose");
-const bodyParser = require('body-parser');
+// Connect to MongoDB
+mongoose.connect('mongodb+srv://plan:plan@cluster0.yuuofm2.mongodb.net/?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-// setting layouts
-app.use(expressLayouts);
+// Create a file schema
+const fileSchema = new mongoose.Schema({
+  fileName: {
+    type: String,
+    required: true,
+  },
+  filePath: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
-// middleware for body-parser
-app.use(bodyParser.json());
-app.use(express.urlencoded({extended: true}));
+// Create a file model
+const File = mongoose.model('File', fileSchema);
 
-//accesing static files from assets folder
-app.use(express.static('./assets'));    
+// Set up file upload using multer
+const upload = multer({ dest: 'uploads/' });
 
-//setting up view engine
-app.set("view engine", "ejs");
-app.set("views", "./views");
+// Serve static files (CSS, JS, etc.)
+app.use(express.static(path.join(__dirname, 'public')));
 
-// setting up routes
-app.use('/', require('./routes'));
+// Serve uploaded files
+app.use('/uploaded_files', express.static(path.join(__dirname, 'uploads')));
 
-// directing the app in the given port 
-app.listen(port, function(err) {
-    if(err) {
-        console.log('Error', err);
-        return;
-    }
-    console.log('Server is up and running on port: ', port);
+// API endpoint to render the home page
+app.get('/', (req, res) => {
+  res.send("786");
+});
 
+// API endpoint to handle file upload
+app.post('/upload', upload.single('file'), async (req, res) => {
+  if (!req.file) {
+    return res.status(400).send('No file uploaded.');
+  }
+
+  // Create a new file record in the database
+  try {
+    const file = new File({
+      fileName: req.file.originalname,
+      filePath: req.file.path,
+    });
+    await file.save();
+    console.log('Uploaded file:', req.file.originalname);
+    return res.status(200).send('File uploaded successfully.');
+  } catch (error) {
+    console.error('Error saving file:', error);
+    return res.status(500).send('Error saving file.');
+  }
+});
+
+// API endpoint to get a list of uploaded files
+app.get('/files', async (req, res) => {
+  try {
+    const files = await File.find().sort({ createdAt: 'desc' });
+    return res.json(files);
+  } catch (error) {
+    console.error('Error retrieving file list:', error);
+    return res.status(500).send('Error retrieving file list.');
+  }
+});
+
+// API endpoint to handle file deletion
+app.get('/delete/:file', async (req, res) => {
+  const fileName = req.params.file;
+  const filePath = path.join('uploads', fileName);
+
+  try {
+    // Delete the file record from the database
+    await File.deleteOne({ fileName: fileName });
+    
+    // Delete the file from the filesystem
+    fs.unlink(filePath, err => {
+      if (err) {
+        console.error('Error deleting file:', err);
+        return res.status(500).send('Error deleting file.');
+      }
+      console.log('Deleted file:', fileName);
+      return res.status(200).send('File deleted successfully.');
+    });
+  } catch (error) {
+    console.error('Error deleting file:', error);
+    return res.status(500).send('Error deleting file.');
+  }
+});
+
+// Start the server
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
 });
